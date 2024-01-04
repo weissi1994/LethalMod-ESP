cbc8a2705d3a099ae6eeaf983a093083be915d6d
ion <ion@n0de.biz>
2024-01-04 16:11:17 +0000

    upd

--- LethalMod/Plugin.cs
+++ LethalMod/Plugin.cs
@@ -20,7 +20,7 @@ namespace LethalMod
             agent.updateUpAxis = false;
         }
 
-        public Vector3[] DrawPath(Vector3 start, Vector3 target, Color color)
+        public void DrawPath(Vector3 start, Vector3 target, Color color)
         {
             var path = new NavMeshPath();
             agent.nextPosition = start;
@@ -41,7 +41,7 @@ namespace LethalMod
               line.SetPosition( i, path.corners[ i ] );
             }
 
-            return path.corners;
+            Destroy(this);
         }
     }
 
@@ -94,10 +94,6 @@ namespace LethalMod
                 local_player = HUDManager.Instance?.localPlayer;
                 if (local_player != null) {
                     camera = local_player.gameplayCamera;
-                    if (local_player.thisPlayerBody.gameObject.GetComponent<PathFinder>() == null) {
-                        Logger.LogWarning($"Attaching PathFinder to local player");
-                        local_player.thisPlayerBody.gameObject.AddComponent<PathFinder>(); 
-                    }
                     // if (NavMesh.SamplePosition(local_player.transform.position, out NavMeshHit hit, 1.0f, NavMesh.AllAreas)) {
                     //     if (camera.gameObject.GetComponent<NavMeshAgent>() == null) {
                     //         Logger.LogWarning($"Attaching NavMeshAgent to local player");
@@ -164,7 +160,11 @@ namespace LethalMod
                 new Vector2(entity_screen_pos.x - box_width / 2, entity_screen_pos.y - box_height / 2), box_width,
                 box_height,
                 color, box_thickness);
-                local_player.thisPlayerBody.gameObject.GetComponent<PathFinder>().DrawPath(local_player.thisPlayerBody.transform.position, entity_position, color);
+                if (camera.gameObject.GetComponent<PathFinder>() == null) {
+                    Logger.LogWarning($"Attaching PathFinder to local player");
+                    camera.gameObject.AddComponent<PathFinder>(); 
+                }
+                camera.gameObject.GetComponent<PathFinder>().DrawPath(camera.transform.position, entity_position, color);
                 //render.draw_line(new Vector2(Screen.width / 2, Screen.height),
                 //new Vector2(entity_screen_pos.x, entity_screen_pos.y + box_height / 2), color, 2f);
             }
