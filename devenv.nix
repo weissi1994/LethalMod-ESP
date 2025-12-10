962b85c80046e0d29984d2a65a64c45a463d9329
ion <daniel@n0de.biz>
2025-12-10 21:04:50 +0000

    bump: 0.0.14

--- devenv.nix
+++ devenv.nix
@@ -9,6 +9,14 @@
   scripts.build.exec = ''
     dotnet build -o release -c Release LethalMod.sln
   '';
+  scripts.release.exec = ''
+    build
+    VERSION=$(cat ./manifest.json | ${pkgs.jq}/bin/jq -r '.version_number')
+    cp ./release/LethalMod.dll ./LethalMod.dll
+    cp ./LethalMod/Plugin.cs ./Source.cs
+    ${pkgs.zip}/bin/zip LethalMod_$VERSION.zip ./LethalMod.dll ./icon.png ./manifest.json ./README.md ./Source.cs
+    rm ./Source.cs ./LethalMod.dll
+  '';
 
   pre-commit.hooks = {
     check-merge-conflicts.enable = true;
